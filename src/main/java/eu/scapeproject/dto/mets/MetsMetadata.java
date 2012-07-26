62f12a6530ddd05ff91be9bc59e7e62a263454d8
fasseg <frank.asseg@googlemail.com>
2012-07-26 12:41:58 +0200

    added ids to metadata

--- src/main/java/eu/scapeproject/dto/mets/MetsMetadata.java
+++ src/main/java/eu/scapeproject/dto/mets/MetsMetadata.java
@@ -1,6 +1,22 @@
 package eu.scapeproject.dto.mets;
 
-
 public abstract class MetsMetadata {
-
+	protected String id;
+	
+	protected MetsMetadata(){
+		super();
+	}
+	
+	protected MetsMetadata(String id){
+		super();
+		this.id=id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	protected void setId(String id) {
+		this.id = id;
+	}
 }

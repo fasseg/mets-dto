44569745173b0f1c6fe71424f6821ff82d5b7e34
fasseg <frank.asseg@googlemail.com>
2012-07-27 11:23:16 +0200

    added versioning support, not METS conform though

--- src/main/java/eu/scapeproject/dto/mets/MetsHeader.java
+++ src/main/java/eu/scapeproject/dto/mets/MetsHeader.java
@@ -10,124 +10,137 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "metsHdr", namespace = "http://www.loc.gov/METS/")
 public class MetsHeader {
-    @XmlAttribute(name = "ID")
-    private String id;
-    @XmlAttribute(name = "ADMID")
-    private String admId;
-    @XmlAttribute(name = "CREATEDATE")
-    private Date createdDate;
-    @XmlAttribute(name = "LASTMODDATE")
-    private Date lastModDate;
-    @XmlAttribute(name = "RECORDSTATUS")
-    private String recordStatus;
-    @XmlElement(name = "agent", namespace = "http://www.loc.gov/METS/")
-    private List<MetsAgent> agents;
-    @XmlElement(name = "altRecordID", namespace = "http://www.loc.gov/METS/")
-    private List<MetsAlternativeIdentifer> alternativeIdentifiers;
+	@XmlAttribute(name = "ID")
+	private String id;
+	@XmlAttribute(name = "ADMID")
+	private String admId;
+	@XmlAttribute(name = "CREATEDATE")
+	private Date createdDate;
+	@XmlAttribute(name = "LASTMODDATE")
+	private Date lastModDate;
+	@XmlAttribute(name = "RECORDSTATUS")
+	private String recordStatus;
+	@XmlAttribute(name = "VERSION")
+	private int versionNumber;
+	@XmlElement(name = "agent", namespace = "http://www.loc.gov/METS/")
+	private List<MetsAgent> agents;
+	@XmlElement(name = "altRecordID", namespace = "http://www.loc.gov/METS/")
+	private List<MetsAlternativeIdentifer> alternativeIdentifiers;
 
-    private MetsHeader() {
-        super();
-    }
+	private MetsHeader() {
+		super();
+	}
 
-    private MetsHeader(Builder builder) {
-        this.id = builder.id;
-        this.admId = builder.admId;
-        this.createdDate = builder.createdDate;
-        this.lastModDate = builder.lastModDate;
-        this.recordStatus = builder.recordStatus;
-        this.agents = builder.agents;
-        this.alternativeIdentifiers = builder.alternativeIdentifiers;
-    }
+	private MetsHeader(Builder builder) {
+		this.id = builder.id;
+		this.admId = builder.admId;
+		this.createdDate = builder.createdDate;
+		this.lastModDate = builder.lastModDate;
+		this.recordStatus = builder.recordStatus;
+		this.agents = builder.agents;
+		this.alternativeIdentifiers = builder.alternativeIdentifiers;
+		this.versionNumber = builder.versionNumber;
+	}
 
-    public String getAdmId() {
-        return admId;
-    }
+	public String getAdmId() {
+		return admId;
+	}
 
-    public Date getCreatedDate() {
-        return createdDate;
-    }
+	public Date getCreatedDate() {
+		return createdDate;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public Date getLastModDate() {
-        return lastModDate;
-    }
+	public Date getLastModDate() {
+		return lastModDate;
+	}
 
-    public String getRecordStatus() {
-        return recordStatus;
-    }
+	public String getRecordStatus() {
+		return recordStatus;
+	}
 
-    public List<MetsAgent> getAgents() {
-        return agents;
-    }
+	public List<MetsAgent> getAgents() {
+		return agents;
+	}
 
-    public List<MetsAlternativeIdentifer> getAlternativeIdentifiers() {
-        return alternativeIdentifiers;
-    }
+	public List<MetsAlternativeIdentifer> getAlternativeIdentifiers() {
+		return alternativeIdentifiers;
+	}
 
-    public static class Builder {
-        private final String id;
-        private String admId;
-        private Date createdDate;
-        private Date lastModDate;
-        private String recordStatus;
-        private List<MetsAgent> agents;
-        private List<MetsAlternativeIdentifer> alternativeIdentifiers;
+	public int getVersionNumber() {
+		return versionNumber;
+	}
 
-        public Builder(String id) {
-            this.id = id;
-        }
+	public static class Builder {
+		private final String id;
+		private String admId;
+		private Date createdDate;
+		private Date lastModDate;
+		private String recordStatus;
+		private int versionNumber;
+		private List<MetsAgent> agents;
+		private List<MetsAlternativeIdentifer> alternativeIdentifiers;
 
-        public Builder addAgent(MetsAgent agent) {
-            if (this.agents == null) {
-                this.agents = new ArrayList<MetsAgent>();
-            }
-            this.agents.add(agent);
-            return this;
-        }
+		public Builder(String id) {
+			this.id = id;
+		}
 
-        public Builder addAlternativeIdentifier(MetsAlternativeIdentifer id) {
-            if (this.alternativeIdentifiers == null) {
-                this.alternativeIdentifiers = new ArrayList<MetsAlternativeIdentifer>();
-            }
-            this.alternativeIdentifiers.add(id);
-            return this;
-        }
+		public Builder addAgent(MetsAgent agent) {
+			if (this.agents == null) {
+				this.agents = new ArrayList<MetsAgent>();
+			}
+			this.agents.add(agent);
+			return this;
+		}
 
-        public Builder agents(List<MetsAgent> agents) {
-            this.agents = agents;
-            return this;
-        }
+		public Builder addAlternativeIdentifier(MetsAlternativeIdentifer id) {
+			if (this.alternativeIdentifiers == null) {
+				this.alternativeIdentifiers = new ArrayList<MetsAlternativeIdentifer>();
+			}
+			this.alternativeIdentifiers.add(id);
+			return this;
+		}
 
-        public Builder admId(String admId) {
-            this.admId = admId;
-            return this;
-        }
+		public Builder agents(List<MetsAgent> agents) {
+			this.agents = agents;
+			return this;
+		}
 
-        public MetsHeader build() {
-            return new MetsHeader(this);
-        }
+		public Builder admId(String admId) {
+			this.admId = admId;
+			return this;
+		}
 
-        public Builder createdDate(Date createdDate) {
-            this.createdDate = createdDate;
-            return this;
-        }
+		public MetsHeader build() {
+			return new MetsHeader(this);
+		}
 
-        public Builder lastModDate(Date lastModDate) {
-            this.lastModDate = lastModDate;
-            return this;
-        }
+		public Builder createdDate(Date createdDate) {
+			this.createdDate = createdDate;
+			return this;
+		}
 
-        public Builder recordStatus(String recordStatus) {
-            this.recordStatus = recordStatus;
-            return this;
-        }
+		public Builder lastModDate(Date lastModDate) {
+			this.lastModDate = lastModDate;
+			return this;
+		}
 
-        public Builder alternativeIdentifiers(List<MetsAlternativeIdentifer> alternativeIdentifiers) {
-            this.alternativeIdentifiers = alternativeIdentifiers;
-            return this;
-        }
-    }
+		public Builder recordStatus(String recordStatus) {
+			this.recordStatus = recordStatus;
+			return this;
+		}
+
+		public Builder versionNumber(int versionNumber) {
+			this.versionNumber = versionNumber;
+			return this;
+		}
+		
+		public Builder alternativeIdentifiers(List<MetsAlternativeIdentifer> alternativeIdentifiers) {
+			this.alternativeIdentifiers = alternativeIdentifiers;
+			return this;
+		}
+	}
 }
---
+++ src/main/java/eu/scapeproject/dto/mets/MetsMDRef.java
@@ -0,0 +1,213 @@
+package eu.scapeproject.dto.mets;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="mdRef",namespace="http://www.loc.gov/METS/")
+public class MetsMDRef {
+    @XmlAttribute(name = "id")
+	private String id;
+    @XmlAttribute(name = "mimeType")
+	private String mimeType;
+    @XmlAttribute(name = "label")
+	private String label;
+    @XmlAttribute(name = "xptr")
+	private String xptr;
+    @XmlAttribute(name = "locType")
+	private String locType;
+    @XmlAttribute(name = "otherLocType")
+	private String otherLocType;
+    @XmlAttribute(name = "mdType")
+	private String mdType;
+    @XmlAttribute(name = "mdTypeVersion")
+	private String mdTypeVersion;
+    @XmlAttribute(name = "otherMdType")
+	private String otherMdType;
+    @XmlAttribute(name = "size")
+	private long size;
+    @XmlAttribute(name = "created")
+	private Date created;
+    @XmlAttribute(name = "checksum")
+	private String checksum;
+    @XmlAttribute(name = "checksumType")
+	private String checksumType;
+    @XmlAttribute(name = "href")
+	private String href;
+
+	@SuppressWarnings("unused")
+	private MetsMDRef() {
+		super();
+	}
+
+	private MetsMDRef(Builder b) {
+		this.id = b.id;
+		this.mimeType = b.mimeType;
+		this.label = b.label;
+		this.xptr = b.xptr;
+		this.locType = b.locType;
+		this.otherLocType = b.otherLocType;
+		this.mdType = b.mdType;
+		this.mdTypeVersion = b.mdTypeVersion;
+		this.otherMdType = b.otherMdType;
+		this.size = b.size;
+		this.created = b.created;
+		this.checksum = b.checksum;
+		this.checksumType = b.checksumType;
+		this.href = b.href;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public String getXptr() {
+		return xptr;
+	}
+
+	public String getLocType() {
+		return locType;
+	}
+
+	public String getOtherLocType() {
+		return otherLocType;
+	}
+
+	public String getMdType() {
+		return mdType;
+	}
+
+	public String getMdTypeVersion() {
+		return mdTypeVersion;
+	}
+
+	public String getOtherMdType() {
+		return otherMdType;
+	}
+
+	public long getSize() {
+		return size;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public String getChecksum() {
+		return checksum;
+	}
+
+	public String getChecksumType() {
+		return checksumType;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public static class Builder {
+		private String id;
+		private String mimeType;
+		private String label;
+		private String xptr;
+		private String locType;
+		private String otherLocType;
+		private String mdType;
+		private String mdTypeVersion;
+		private String otherMdType;
+		private long size;
+		private Date created;
+		private String checksum;
+		private String checksumType;
+		private String href;
+
+		public Builder() {
+			super();
+		}
+
+		public MetsMDRef build() {
+			return new MetsMDRef(this);
+		}
+
+		public Builder id(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder mimeType(String mimeType) {
+			this.mimeType = mimeType;
+			return this;
+		}
+
+		public Builder label(String label) {
+			this.label = label;
+			return this;
+		}
+
+		public Builder xptr(String xptr) {
+			this.xptr = xptr;
+			return this;
+		}
+
+		public Builder locType(String locType) {
+			this.locType = locType;
+			return this;
+		}
+
+		public Builder otherLocType(String otherLocType) {
+			this.otherLocType = otherLocType;
+			return this;
+		}
+
+		public Builder mdType(String mdType) {
+			this.mdType = mdType;
+			return this;
+		}
+
+		public Builder mdTypeVersion(String mdTypeVersion) {
+			this.mdTypeVersion = mdTypeVersion;
+			return this;
+		}
+
+		public Builder otherMdType(String otherMdType) {
+			this.otherMdType = otherMdType;
+			return this;
+		}
+
+		public Builder checksum(String checksum) {
+			this.checksum = checksum;
+			return this;
+		}
+
+		public Builder checksumType(String checksumType) {
+			this.checksumType = checksumType;
+			return this;
+		}
+
+		public Builder href(String href) {
+			this.href = href;
+			return this;
+		}
+
+		public Builder created(Date created) {
+			this.created = created;
+			return this;
+		}
+
+		public Builder size(long size) {
+			this.size = size;
+			return this;
+		}
+	}
+
+}
